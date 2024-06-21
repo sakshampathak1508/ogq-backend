0dfb3953083bcc627a2f8188a71e8e65f9b1f996
saksham15 <saksham.pathak@pocketfm.com>
2024-06-22 00:37:39 +0530

    correct DB

--- ogq/settings.py
+++ ogq/settings.py
@@ -85,23 +85,23 @@ WSGI_APPLICATION = 'ogq.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'ogqbackend$ogq-db',
-#         'USER': 'ogqbackend',
-#         'PASSWORD': 'ogq@2024',
-#         'HOST': 'ogqbackend.mysql.pythonanywhere-services.com',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ogqbackend$ogq-db',
+        'USER': 'ogqbackend',
+        'PASSWORD': 'ogq@2024',
+        'HOST': 'ogqbackend.mysql.pythonanywhere-services.com',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
